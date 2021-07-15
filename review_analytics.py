7b6638f188d1330b710c0dd99c8a5eb49e7ef694
Li ChengYang <sinnerunoclay@gmail.com>
2021-07-15 12:46:58 +0800

    second version

--- review_analytics.py
+++ review_analytics.py
@@ -1,12 +1,19 @@
 # 留言分析程式
 
-data = []
+datas = []
 count = 0
+length = 0
+
 with open('reviews.txt', 'r') as f:
 	for line in f:
-		data.append(line)
+		datas.append(line)
 		count += 1
 		if count % 1000 == 0:
-			print(len(data))
-			
-print(data[0])
+			print(len(datas))
+
+for data in datas:
+	length += len(data)
+
+
+print('檔案讀取完了，總共有', len(datas), '筆資料喔~')
+print('每筆留言的平均長度為', length / len(datas))
